9e46df1b719b38efac670d2d7d93ccb284095856
ClarenceKim <tkarnr0926@gmail.com>
2018-06-06 19:30:32 +0900

    no message

---
+++ joinForm/app.js
@@ -0,0 +1,72 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var join = require('./routes/joinForm');
+var board = require('./routes/board');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var busboy = require('connect-busboy');
+var session = require('express-session');
+var bcrypt = require('bcrypt-nodejs'); // 3장에서 사용할 암호화 모듈
+var cookieSession = require('cookie-session')
+
+
+//var upload = multer({dest: './uploads/'});
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+
+}));
+
+
+app.use(express.static('public'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(busboy());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/join', join);
+app.use('/board', board);
+//app.use(express.methodOverride());
+//app.use(express.multipart());
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(multer({dest:'./uploads/'}).single('file'));
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+module.exports = app;
---
+++ joinForm/routes/joinForm.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.render('joinForm', {title: 'Join Form!'});
+});
+
+router.post('/', function (req, res, next) {
+  //  console.log('req.body : ' + req.body);
+    res.json(req.body);
+});
+module.exports = router;
