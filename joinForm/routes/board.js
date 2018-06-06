51d85141c8e61026ecc16bcfec413549967827aa
muuzzii <37859364+muuzzii@users.noreply.github.com>
2018-06-06 19:37:34 +0900

    Update board.js

--- joinForm/routes/board.js
+++ joinForm/routes/board.js
@@ -22,145 +22,6 @@ router.get('/', function (req, res, next) {
   res.render('index',{user_id : sess});
 });
 
-
-
-
-/*
-router.get('/list/:page', function(req, res, next) {
-  pool.getConnection(function (err,connection){
-    if (err) throw err;
-    var sqlForSelectList = "SELECT idx, creator_id, title, hit FROM board";
-    connection.query(sqlForSelectList,function(err,rows){
-      if(err) console.error("err : "+err);
-      console.log("rows : "+JSON.stringify(rows));
-      res.render('list', { title: '게시판 전체 글 조회',rows: rows });
-      connection.release();
-    });
-  });
-});
-
-router.get('/read/:idx',function(req,res,next){
-  var idx = req.params.idx;
-  pool.getConnection(function(err, connection){
-    var sql = "select idx, creator_id, title, content, img, hit from board where idx=?";
-    connection.query(sql,[idx],function(err, row){
-      if(err) console.error(err);
-      console.log("rows : "+JSON.stringify(row));
-      //console.log("1개 글 조회 결과 확인 : "+row);
-      res.render('read',{title : "글 조회",row:row[0]});
-      connection.release();
-    });
-  });
-});
-
-router.get('/write', function (req, res, next) {
-    res.render('write',{title :" 게시판 글 쓰기"});
-});
-
-
-router.post('/write', upload.single('img'), function(req,res){
-  var creator_id = req.body.creator_id;
-  var title = req.body.title;
-  var content = req.body.content;
-  var passwd = req.body.passwd;
-  var uploadCnt = 0;
-  var upFile = req.file;
-  var file_name = '/images/'
-  var mimetype = upFile.mimetype.split('/')[1];
-  file_name += upFile.filename;
-  file_name += '.';
-  file_name += mimetype;
-
-  var oldName = __dirname+'/../uploads/'+upFile.filename;
-  var path =  __dirname+'/../public'+file_name;
-  fs.rename(oldName,path, function (err) { if (err) throw err; console.log('renamed complete'); });
-
-
-
-  var datas = [creator_id, title, content, passwd, file_name, path];
-
-  console.log('upFile : '+ file_name);
-  pool.getConnection(function (err,connection){
-    var sqlForInsertBoard = "insert into board(creator_id, title, content, passwd, img, path) values(?, ?, ?, ?, ?, ?)";
-    connection.query(sqlForInsertBoard,datas,function(err, rows){
-      if(err) console.error("err : "+err);
-      console.log("rows : " + JSON.stringify(rows));
-      console.log('------------img : ' + file_name);
-      if(file_name == '')
-       console.log('OK !!!!!!!!!!!!!!!!');
-      else
-       console.log('NO !!!!!!!!!!!!!!!!');
-
-      res.redirect('/board/list/1');
-      connection.release();
-    });
-  });
-
-});
-
-// 클수정 화면 표시 GET
-router.get('/update',function(req,res,next){
-  var idx = req.query.idx;
-
-  pool.getConnection(function(err, connection){
-    if(err) console.error("커넥션 객체 얻어오기 에러 : "+err);
-    var sql = "select idx, creator_id, title, content, hit from board where idx=?";
-    connection.query(sql,[idx],function(err, rows){
-      if(err) console.error(err);
-      console.log("update에서 1개 글 조회 결과 확인 : "+rows);
-      res.render('update',{title : "글 조회",row:rows[0]});
-
-      connection.release();
-    });
-  });
-});
-
-//글수정 로직 처리 post
-router.post('/update', function(req,res,next){
-  var idx = req.body.idx;
-  var creator_id = req.body.creator_id;
-  var title = req.body.title;
-  var content = req.body.content;
-  var passwd = req.body.passwd;
-
-  var datas = [creator_id,title,content,passwd];
-  pool.getConnection(function (err,connection){
-    var sql = "update board set creator_id=?, title=?, content=? where idx=? and passwd=?";
-    connection.query(sql,[creator_id,title,content,idx,passwd],function(err, result){
-      console.log(result);
-      if(err) console.error("글 수정 중 에러 발생 err : "+err);
-
-      if(result.affectedRows==0){
-        res.send("<script>alert('패스워드가 일치하지 않거나, 잘못된 요청으로 인해 값이 변경되지 않았습니다.');history.back();</script>" );
-      }
-      else{
-        res.redirect('/board/read/'+idx);
-      }
-      connection.release();
-    });
-  });
-});
-
-router.post('/delete', function(req,res,next){
-  var idx = req.body.idx;
-  var passwd = req.body.passwd;
-  pool.getConnection(function (err,connection){
-    var sql = "delete from board where idx=? and passwd=?";
-    connection.query(sql,[idx,passwd],function(err, result){
-      console.log(result);
-      if(err) console.error("글 수정 중 에러 발생 err : "+err);
-
-      if(result.affectedRows==0){
-        res.send("<script>alert('패스워드가 일치하지 않거나, 잘못된 요청으로 인해 값이 삭제되지 않았습니다.');history.back();</script>" );
-      }
-      else{
-        res.redirect('/board/list/1');
-      }
-      connection.release();
-    });
-  });
-});
-*/
 router.get('/products',function(req,res,next){
   var sess = req.session.user_id;
   var ok_admin = "tkarnr0926@gmail.com";
@@ -334,7 +195,7 @@ router.post('/single', function(req, res, next) {
 
   pool.getConnection(function(err, connection)
   {
-    var sql = "select name, price, img_link1, img_link2 from product where name=?";//product db name 중에서 key를 찾기
+    var sql = "select name, price, img_link1, img_link2, description, review from product where name=?";//product db name 중에서 key를 찾기
     connection.query(sql, [name], function(err, row){
       if(err)
       {
@@ -349,10 +210,12 @@ router.post('/single', function(req, res, next) {
         console.log('@@@@@@@@@@@@@', row[0].price);
         console.log('@@@@@@@@@@@@@', row[0].img_link1);
         console.log('@@@@@@@@@@@@@', row[0].img_link2);
+        console.log(row[0].description);
+        console.log(row[0].review)
 
        //그 제품 창 띄워줘야함..!
         //res.redirect('/board/' + row[0].name);
-        res.render('single',{user_id : sess, admin : ok_admin, name : row[0].name, price : row[0].price, img_link1 : row[0].img_link1, img_link2 : row[0].img_link2});
+        res.render('single',{user_id : sess, admin : ok_admin, name : row[0].name, price : row[0].price, img_link1 : row[0].img_link1, img_link2 : row[0].img_link2, description : row[0].description, review : row[0].review});
         //res.redirect('/board/single')
       }
       connection.release();
@@ -867,7 +730,7 @@ router.post('/p_single', function(req, res, next) {
   if(sess!=ok_admin) ok_admin = "no"
   pool.getConnection(function(err, connection)
   {
-    var sql = "select name, price, img_link1, img_link2 from product where name=?";//product db name 중에서 key를 찾기
+    var sql = "select name, price, img_link1, img_link2, description, review from product where name=?";//product db name 중에서 key를 찾기
     connection.query(sql, [name], function(err, row){
       if(err)
       {
@@ -885,7 +748,7 @@ router.post('/p_single', function(req, res, next) {
 
        //그 제품 창 띄워줘야함..!
         //res.redirect('/board/' + row[0].name);
-        res.render('p_single',{user_id : sess, admin : ok_admin, name : row[0].name, price : row[0].price, img_link1 : row[0].img_link1, img_link2 : row[0].img_link2});
+        res.render('p_single',{user_id : sess, admin : ok_admin, name : row[0].name, price : row[0].price, img_link1 : row[0].img_link1, img_link2 : row[0].img_link2, description : row[0].description, review : row[0].review});
         //res.redirect('/board/single')
       }
       connection.release();
